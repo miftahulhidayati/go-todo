90cd13c115dadbbcbce39bd8fc2990add860b09e
Miftahul Hidayati <miftahulhdyt@gmail.com>
2021-12-09 23:41:13 +0700

    change swaggo config host

--- docs/docs.go
+++ docs/docs.go
@@ -597,10 +597,10 @@ type swaggerInfo struct {
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
 	Version:     "1.0",
-	Host:        "localhost:8080",
+	Host:        "go-todo-miftahulhdyt.herokuapp.com/",
 	BasePath:    "/api/v1",
 	Schemes:     []string{},
-	Title:       "API Todo Application",
+	Title:       "API Todo Applications",
 	Description: "This is a Todo API.",
 }
 
--- main.go
+++ main.go
@@ -32,7 +32,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
+// @host go-todo-miftahulhdyt.herokuapp.com/
 // @BasePath /api/v1
 
 // @securityDefinitions.apikey BearerAuth
