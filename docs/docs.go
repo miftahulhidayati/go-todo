fb9297e65e4c8fff7b5e6e069e15f0e804f3abae
Miftahul Hidayati <miftahulhdyt@gmail.com>
2021-12-09 23:58:09 +0700

    fixing base url swagger

--- docs/docs.go
+++ docs/docs.go
@@ -597,7 +597,7 @@ type swaggerInfo struct {
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
 	Version:     "1.0",
-	Host:        "go-todo-miftahulhdyt.herokuapp.com/",
+	Host:        "go-todo-miftahulhdyt.herokuapp.com",
 	BasePath:    "/api/v1",
 	Schemes:     []string{},
 	Title:       "API Todo Applications",
